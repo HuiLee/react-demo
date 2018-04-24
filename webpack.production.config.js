b960eb9d9972e821dac9eb7cc1a2ae11b3bf992a
Hui Lee <lihui870920@gmail.com>
2018-04-24 20:26:11 +0800

    Compress JS Files

---
+++ webpack.production.config.js
@@ -0,0 +1,38 @@
+const pkg = require('./package.json')
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const root = __dirname
+module.exports = {
+    mode: 'production',
+    entry: {
+        app:path.resolve(root, 'app/main.js'),
+        vendor:Object.keys(pkg.dependencies),
+    },
+    output: {
+        path:path.resolve(__dirname, 'build'),
+        filename: '[name].[hash].bundle.js',
+        publicPath: '/'
+    },
+    resolve:{
+        extensions: ['.js','.jsx']
+    },
+    module: {
+        rules: [
+            {test: /\.(js|jsx)?$/, use: ['babel-loader'], exclude: /node_modules/},
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'],
+                exclude: /node_modules/
+            },
+            {test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'], exclude: /node_modules/},
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(root, 'template.html')
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+
+    ]
+}
